5427dd51b0cb74f79445cd5ff339c2831c8c0411
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-24 16:20:06 +0800

    Added update functions for the muscle and exercise tables

--- models/queries.js
+++ models/queries.js
@@ -1,7 +1,18 @@
 const pool = require("./pool");
 
 
-//GET query functions
+//CREATE query functions (POST)
+
+async function addMuscle(muscleName){
+    await pool.query("INSERT INTO muscles (muscle_group_name) VALUES ($1)", [muscleName]);
+}
+
+async function addExercise(exerciseName){
+    await pool.query("INSERT INTO exercises(exercise_name VALUES ($1)", [exerciseName]);
+}
+
+
+//READ query functions (GET)
 async function getAllMuscles(){
     const { rows } = await pool.query(`SELECT muscle_group_name AS "Muscle Name" FROM muscles;`);
     return rows;
@@ -27,12 +38,25 @@ async function getAllExerciseMuscles(){
     return rows;
 }
 
-//POST query functions
+//UPDATE query functions (PUT)
 
-async function addMuscle(muscleName){
-    await pool.query("INSERT INTO muscles (muscle_group_name) VALUES ($1)", [muscleName]);
+async function updateMuscle(muscleName, newMuscleName){
+    const SQL = `
+        UPDATE muscles
+        SET muscle_name = ${newMuscleName}
+        WHERE muscle_name = ${muscleName};
+    `
+
+    await pool.query(SQL);
 }
 
-async function addExercise(exerciseName){
-    await pool.query("INSERT INTO exercises(exercise_name VALUES ($1)", [exerciseName]);
+async function updateExercise(exerciseName, newExerciseName){
+    const SQL = `
+        UPDATE exercises
+        SET exercise_name = ${newExerciseName}
+        WHERE exercise_name = ${exercise_name};
+    `
+
+    await pool.query(SQL);
 }
+
