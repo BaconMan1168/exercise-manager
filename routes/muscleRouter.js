4f1bfe2685032222503e7382984e1f746526414d
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-26 16:53:27 +0800

    Added route in muscleRouter for finding muscles by exercise

--- routes/muscleRouter.js
+++ routes/muscleRouter.js
@@ -6,5 +6,6 @@ const muscleRouter = Router();
 muscleRouter.get('/', muscleController.homeMusclePage);
 
 muscleRouter.get('/search', muscleController.readMusclesByName);
+muscleRouter.get('/search-by-exercise', muscleController.readMusclesByExercise);
 
 module.exports = muscleRouter;
