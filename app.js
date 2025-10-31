b3a62d6583f191d2ccf01a2ba38c383f30e91249
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-31 21:19:06 +0800

    Added routers to app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('node:path')
 
 const indexRouter = require('./routes/indexRouter')
 const muscleRouter = require('./routes/muscleRouter')
+const exerciseRouter = require('./routes/exerciseRouter')
+const pairsRouter = require('./routes/exerciseMuscleRouter')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,3 +16,13 @@ app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/muscles', muscleRouter);
+app.use('/exercises', exerciseRouter);
+app.use('/pairs', pairsRouter);
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Exercise Manager - listening on port ${PORT}!`);
+});
