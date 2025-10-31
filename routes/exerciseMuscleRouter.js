444a0d143abeecd6ff43c3710ed263e39797336b
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-10-31 20:36:57 +0800

    Added routes to the exercisePair router

---
+++ routes/exerciseMuscleRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const exerciseMuscleController = require('../controllers/exerciseMuscleController')
+
+const pairsRouter = Router();
+
+exerciseMuscleController.get('/', exerciseMuscleController.homeExerciseMusclePairsPage);
+
+exerciseRouter.post('/add', exerciseMuscleController.createExerciseMusclePair);
+
+
+module.exports = muscleRouter;
