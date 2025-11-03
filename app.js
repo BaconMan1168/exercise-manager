746560002a876d2e8283859f8fff6ddc8327aad9
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-11-03 18:16:04 +0800

    Fixed error with method override not working for the muscle forms

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('node:path')
 const methodOverride = require('method-override')
+app.use(methodOverride('_method'));
 
 const indexRouter = require('./routes/indexRouter')
 const muscleRouter = require('./routes/muscleRouter')
@@ -10,9 +11,8 @@ const pairsRouter = require('./routes/exerciseMuscleRouter')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
